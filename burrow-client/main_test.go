e3211e5acfebf267dd5465d17a1b8595bdff4a63
E. Fynn <me@enriquefynn.com>
2019-02-04 18:20:17 +0100

    burrow client impl

---
+++ burrow-client/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/hyperledger/burrow/crypto"
+)
+
+func TestDeployContract(t *testing.T) {
+	addr, err := crypto.AddressFromHexString("0000000000000000000000000000000000000000")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+	DeployContract(addr, 0, "binaries/scalable_coin.bin/ScalableCoin.bin")
+}
