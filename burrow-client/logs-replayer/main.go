1eb12178a749bfce1caa210d60ef687b44bc5d8d
E. Fynn <me@enriquefynn.com>
2019-03-13 17:02:57 +0100

    files for logtesting

---
+++ burrow-client/logs-replayer/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/hyperledger/burrow/acm"
+	"github.com/hyperledger/burrow/crypto"
+	"github.com/hyperledger/burrow/deploy/def"
+	"github.com/hyperledger/burrow/rpc/rpcevents"
+	"github.com/hyperledger/burrow/txs"
+
+	"github.com/enriquefynn/sharding-runner/burrow-client/utils"
+	"github.com/sirupsen/logrus"
+	yaml "gopkg.in/yaml.v2"
+)
+
+func checkFatalError(err error) {
+	if err != nil {
+		logrus.Fatalf("Error: %v", err)
+	}
+}
+
+func getTransaction(config *utils.Config, logsReader *LogsReader) (<-chan *txs.Envelope, error) {
+	replayLog := config.Contracts.ReplayTransactionsPath
+	logrus.Infof("Logs path: %v", replayLog)
+
+	chnl := make(chan *txs.Envelope)
+	go func() {
+		for {
+			tx, err := logsReader.LoadLog()
+			chnl <- tx
+			if err != nil {
+				logrus.Infof("Stopping reading txs: %v", err)
+				close(chnl)
+				break
+			}
+		}
+	}()
+	return chnl, nil
+}
+
+func clientReplayer(client *def.Client, tx *txs.Envelope) {
+	resp, err := client.BroadcastEnvelope(tx)
+	fatalError(err)
+	if resp.Exception != nil {
+		logrus.Fatalf("Exception in tx: %v : %v", tx, resp.Exception)
+	}
+	logrus.Infof("Resp: %v", resp.Exception)
+	logrus.Infof("Resp: %v", resp.Events[1].Log.Data)
+}
+
+func clientEmitter(client *def.Client, txChannel <-chan *txs.Envelope) {
+	txs := 0
+	startTime := time.Now()
+	for tx := range txChannel {
+		txData := tx.Tx.Payload.Any().CallTx.Data
+		logrus.Infof("INPUT: %v", txData)
+		go clientReplayer(client, tx)
+
+		// logrus.Infof("FROM: %v", to)
+		// go client.BroadcastEnvelope(tx)
+		// time.Sleep(1000000)
+
+		duration := time.Now().Sub(startTime).Seconds()
+		txs++
+		if duration >= 1 {
+			logrus.Infof("[CLI] Txs/s: %v", txs)
+			txs = 0
+			startTime = time.Now()
+		}
+	}
+}
+
+func createContract(config *utils.Config, accounts *LogsReader, client *def.Client, path string, args ...interface{}) (*crypto.Address, error) {
+	contractEnv, err := accounts.CreateContract(path, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	receipt, err := client.BroadcastEnvelope(contractEnv)
+	if err != nil {
+		return nil, err
+	}
+	if receipt.Exception != nil {
+		return nil, err
+	}
+	contract := receipt.Receipt.ContractAddress
+	account, err := client.GetAccount(contract)
+	if len(account.Code) == 0 {
+		return nil, fmt.Errorf("Contract creation failed : %v", account)
+	}
+	return &contract, nil
+}
+
+func listenBlockHeaders(client *def.Client) {
+	end := rpcevents.StreamBound()
+
+	request := &rpcevents.BlocksRequest{
+		BlockRange: rpcevents.NewBlockRange(rpcevents.AbsoluteBound(1), end),
+	}
+	clientEvents, err := client.Query()
+	signedHeaders, err := clientEvents.ListSignedHeaders(context.Background(), request)
+	checkFatalError(err)
+	// firstBlock, err := signedHeaders.Recv()
+	// checkFatalError(err)
+	startTime := time.Now()
+	totalTxs := int64(0)
+	commence := false
+	for {
+		resp, err := signedHeaders.Recv()
+		checkFatalError(err)
+		if !commence && resp.SignedHeader.NumTxs > 0 {
+			logrus.Infof("Commence at: %v %v", resp.SignedHeader.Height, resp.SignedHeader.Time)
+			commence = true
+			startTime = resp.SignedHeader.Time
+		}
+		if commence {
+			elapsedTime := resp.SignedHeader.Time.Sub(startTime)
+			startTime = resp.SignedHeader.Time
+			totalTxs += resp.SignedHeader.NumTxs
+			logrus.Infof("[SRV] Txs: %v, elapsed time: %v", totalTxs, elapsedTime)
+			logrus.Infof("[SRV] Tx/s: %v", float64(resp.SignedHeader.NumTxs)/elapsedTime.Seconds())
+		}
+	}
+}
+
+func main() {
+	config := utils.Config{}
+	configFile, err := ioutil.ReadFile(os.Args[1])
+	checkFatalError(err)
+	err = yaml.Unmarshal(configFile, &config)
+	checkFatalError(err)
+	// Chain id: 1
+	logsReader := CreateLogsReader(config.Benchmark.ChainID, config.Contracts.ReplayTransactionsPath, config.Contracts.CKABI)
+
+	defaultAccount := acm.SigningAccounts([]*acm.PrivateAccount{acm.GeneratePrivateAccountFromSecret("0")})
+	client := def.NewClientWithLocalSigning(config.Benchmark.Address, time.Duration(config.Benchmark.Timeout)*time.Second, defaultAccount)
+
+	// Deploy Genes contract
+	address, err := createContract(&config, logsReader, client, config.Contracts.GenePath)
+	checkFatalError(err)
+	logrus.Infof("Deployed GeneScience at: %v", address)
+
+	// Deploy CK contract
+	address, err = createContract(&config, logsReader, client, config.Contracts.Path, address)
+	logsReader.Advance(2)
+	checkFatalError(err)
+	logrus.Infof("Deployed CK at: %v", address)
+	logsReader.SetContractAddr(address)
+
+	txChannel, err := getTransaction(&config, logsReader)
+	checkFatalError(err)
+
+	go listenBlockHeaders(client)
+	clientEmitter(client, txChannel)
+}
---
+++ burrow-client/logs-replayer/utils.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/hyperledger/burrow/acm"
+	"github.com/hyperledger/burrow/crypto"
+	"github.com/hyperledger/burrow/txs/payload"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hyperledger/burrow/txs"
+)
+
+func fatalError(err error) {
+	if err != nil {
+		// logrus.Fatalf("Error: %v", err)
+	}
+}
+
+type LogsReader struct {
+	logsPath     string
+	logsFile     *os.File
+	logsReader   *bufio.Reader
+	abi          abi.ABI
+	contractAddr *crypto.Address
+	Accounts
+}
+
+func CreateLogsReader(chainID string, path string, abiPath string) *LogsReader {
+	file, err := os.Open(path)
+	fatalError(err)
+	// CK ABI
+	ckABIjson, err := os.Open(abiPath)
+	fatalError(err)
+	ckABI, err := abi.JSON(ckABIjson)
+
+	reader := bufio.NewReader(file)
+	return &LogsReader{
+		logsPath:   path,
+		logsFile:   file,
+		logsReader: reader,
+		abi:        ckABI,
+		Accounts: Accounts{
+			chainID:    chainID,
+			accountMap: make(map[common.Address]*SeqAccount),
+			allowedMap: make(map[crypto.Address]map[int64]crypto.Address),
+		},
+	}
+}
+
+func (lr *LogsReader) SetContractAddr(addr *crypto.Address) {
+	lr.contractAddr = addr
+}
+
+func (lr *LogsReader) Advance(n int) {
+	for i := 0; i < n; i++ {
+		lr.logsReader.ReadString('\n')
+	}
+}
+
+var l int = 3
+
+func (lr *LogsReader) LoadLog() (*txs.Envelope, error) {
+	// logrus.Infof("LINE: %v", l)
+	l++
+	var fromAcc *SeqAccount
+	tx := &payload.CallTx{
+		Address:  lr.contractAddr,
+		Fee:      1,
+		GasLimit: 4100000000,
+	}
+
+	line, err := lr.logsReader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	splitLine := strings.Split(line, " ")
+	// 0      1           2            3         4          5          6         7        8        9          10
+	// Birth owner <addr [20]byte> kittyId <kID uint32> matronId <mID uint32> sireId <sID uint32> genes <genes uint256>
+	if splitLine[0] == "Birth" {
+		owner := common.HexToAddress(splitLine[2])
+		simulatedOwner := lr.getOrCreateAccount(owner)
+		matronID, err := strconv.ParseInt(splitLine[6], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		sireID, err := strconv.Atoi(splitLine[8])
+		if err != nil {
+			return nil, err
+		}
+		genes, succ := big.NewInt(0).SetString(splitLine[10], 10)
+		if !succ {
+			return nil, err
+		}
+
+		// Should call createPromoKitty(uint256 _genes, address _owner)
+		if matronID == 0 && sireID == 0 {
+			// From contract owner
+			fromAcc = lr.getOrCreateAccount(common.BigToAddress(common.Big0))
+			simulatedOwner = lr.getOrCreateAccount(common.BigToAddress(common.Big0))
+			// logrus.Infof("createPromoKitty")
+			txInput, err := lr.abi.Methods["createPromoKitty"].Inputs.Pack(genes, simulatedOwner.account.GetAddress())
+			if err != nil {
+				return nil, err
+			}
+			tx.Data = append(lr.abi.Methods["createPromoKitty"].Id(), txInput...)
+		} else {
+			// From simulated owner (givin birth)
+			fromAcc = simulatedOwner
+			// logrus.Infof("giveBirth")
+			// Should call giveBirth(uint256 _matronId)
+			txInput, err := lr.abi.Methods["giveBirth"].Inputs.Pack(big.NewInt(matronID))
+			if err != nil {
+				return nil, err
+			}
+			tx.Data = append(lr.abi.Methods["giveBirth"].Id(), txInput...)
+		}
+		// Consume Transfer event
+		lr.logsReader.ReadString('\n')
+		l++
+
+		// 0          1           2             3          4        5         6                7          8
+		// Pregnant owner <addr [20]byte>  matronId <mID uint32> sireId <sID uint32> cooldownEndBlock <cooldownEndBlock uint32>
+	} else if splitLine[0] == "Pregnant" {
+		// logrus.Infof("breed")
+		owner := common.HexToAddress(splitLine[2])
+		simulatedOwner := lr.getOrCreateAccount(owner)
+		matronID, err := strconv.ParseInt(splitLine[4], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		sireID, err := strconv.ParseInt(splitLine[6], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		// Should call breed(uint256 _matronId, uint256 _sireId)
+		txInput, err := lr.abi.Methods["breed"].Inputs.Pack(big.NewInt(matronID), big.NewInt(sireID))
+		tx.Data = append(lr.abi.Methods["breed"].Id(), txInput...)
+		if err != nil {
+			return nil, err
+		}
+		fromAcc = simulatedOwner
+		//              0      1    2    3    4      5        6
+		// Approval/Transfer from <addr> to <addr> tokenId <tokenID>
+	} else if splitLine[0] == "Transfer" || splitLine[0] == "Approval" {
+		fr := common.HexToAddress(splitLine[2])
+		to := common.HexToAddress(splitLine[4])
+		simulatedTo := lr.getOrCreateAccount(to)
+		simulatedFrom := lr.getOrCreateAccount(fr)
+
+		tokenID, err := strconv.ParseInt(splitLine[6], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		if splitLine[0] == "Approval" {
+			fromAcc = simulatedFrom
+			// logrus.Infof("approve")
+			lr.addAllowed(simulatedFrom.account.GetAddress(), simulatedTo.account.GetAddress(), tokenID)
+			txInput, err := lr.abi.Methods["approve"].Inputs.Pack(simulatedTo.account.GetAddress(), big.NewInt(tokenID))
+			tx.Data = append(lr.abi.Methods["approve"].Id(), txInput...)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Should call transferFrom(address _from, address _to, uint256 _tokenId)
+			if lr.isAllowed(simulatedFrom.account.GetAddress(), tokenID) {
+				fromAllowed := lr.allowedMap[simulatedFrom.account.GetAddress()][tokenID].Bytes()
+				fromAcc = lr.getOrCreateAccount(common.BytesToAddress(fromAllowed))
+				// logrus.Infof("transferFrom")
+				txInput, err := lr.abi.Methods["transferFrom"].Inputs.Pack(simulatedFrom.account.GetAddress(), simulatedTo.account.GetAddress(), big.NewInt(tokenID))
+				if err != nil {
+					return nil, err
+				}
+				tx.Data = append(lr.abi.Methods["transferFrom"].Id(), txInput...)
+				lr.deleteAllowed(simulatedTo.account.GetAddress(), tokenID)
+				// Should call transfer(address _to, uint256 _tokenId))
+			} else {
+				fromAcc = simulatedFrom
+				// logrus.Infof("transfer")
+				txInput, err := lr.abi.Methods["transfer"].Inputs.Pack(simulatedTo.account.GetAddress(), big.NewInt(tokenID))
+				if err != nil {
+					return nil, err
+				}
+				tx.Data = append(lr.abi.Methods["transfer"].Id(), txInput...)
+			}
+		}
+	} else {
+		return nil, fmt.Errorf("Error, unknown event %v", splitLine[0])
+	}
+
+	txPayload := payload.Payload(tx)
+	fromAcc.sequence++
+	tx.Input = &payload.TxInput{
+		Address:  fromAcc.account.GetAddress(),
+		Amount:   1,
+		Sequence: fromAcc.sequence,
+	}
+
+	env := txs.Enclose(lr.chainID, txPayload)
+	err = env.Sign(fromAcc.account)
+
+	return env, err
+}
+
+type SeqAccount struct {
+	account  acm.AddressableSigner
+	sequence uint64
+}
+
+type Accounts struct {
+	chainID       string
+	accountMap    map[common.Address]*SeqAccount
+	lastAccountID int
+	allowedMap    map[crypto.Address]map[int64]crypto.Address
+}
+
+func (ac *Accounts) getOrCreateAccount(addr common.Address) *SeqAccount {
+	if val, ok := ac.accountMap[addr]; ok {
+		return val
+	}
+	acc := acm.GeneratePrivateAccountFromSecret(strconv.Itoa(ac.lastAccountID))
+	ac.accountMap[addr] = &SeqAccount{
+		account: acm.SigningAccounts([]*acm.PrivateAccount{acc})[0],
+	}
+	ac.lastAccountID++
+	return ac.accountMap[addr]
+
+}
+
+func (ac *Accounts) addAllowed(from, to crypto.Address, tokenID int64) {
+	if _, ok := ac.allowedMap[from]; !ok {
+		ac.allowedMap[from] = make(map[int64]crypto.Address)
+	}
+	ac.allowedMap[from][tokenID] = to
+}
+
+func (ac *Accounts) deleteAllowed(addr crypto.Address, tokenID int64) {
+	delete(ac.allowedMap[addr], tokenID)
+}
+
+func (ac *Accounts) isAllowed(addr crypto.Address, tokenID int64) bool {
+	val, ok := ac.allowedMap[addr]
+	if !ok {
+		return false
+	}
+	_, ok = val[tokenID]
+	if ok {
+		return true
+	}
+	return false
+}
+
+// CreateContract creates a contract with the binary in path
+func (lr *LogsReader) CreateContract(codePath string, args ...interface{}) (*txs.Envelope, error) {
+	var byteArgs []byte
+	var err error
+	if len(args) != 0 {
+		byteArgs, err = lr.abi.Constructor.Inputs.Pack(args...)
+		if err != nil {
+			return nil, err
+		}
+	}
+	acc := lr.getOrCreateAccount(common.BigToAddress(common.Big0))
+	contractContents, err := ioutil.ReadFile(codePath)
+	if err != nil {
+		return nil, err
+	}
+
+	contractHex, err := hex.DecodeString(string(contractContents))
+	if err != nil {
+		return nil, err
+	}
+
+	tx := payload.Payload(&payload.CallTx{
+		Input: &payload.TxInput{
+			Address:  acc.account.GetAddress(),
+			Amount:   1,
+			Sequence: acc.sequence + 1,
+		},
+		Data:     append(contractHex, byteArgs...),
+		Address:  nil,
+		Fee:      1,
+		GasLimit: 4100000000,
+	})
+
+	env := txs.Enclose(lr.chainID, tx)
+	err = env.Sign(acc.account)
+	acc.sequence++
+
+	return env, err
+}
